f59ad6e0ff200f359d912bb9baeeaefb6c69ca66
egzamin <arturprzybylski.plgit config --global user.name artur>
2019-12-17 09:34:11 +0100

    Petla until oraz select

---
+++ bash/z6.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#petla until
+
+i=10
+
+until [ ${i} -eq 10 ]; do
+    echo ${i}
+    i=$[i+1]
+
+done
+
+#petla select
+
+echo "podaj opcje: "
+
+select opcja in File Edit Quit
+do
+    case ${opcja} in
+
+	"File") echo "Wybrano opcje ${opcja}" ;;
+	"Edit") echo "wybrano opcje ${opcja}" ;;
+	"Quit") exit ;;
+    *) "Podaj liczy z zakresu [1..3]" ;;
+    esac
+done
